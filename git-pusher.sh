bcaa659268867b3cce5c0c912092689b2e986693
Ubuntu <ubuntu@ip-172-26-11-8.eu-west-2.compute.internal>
2024-11-15 10:52:00 -0800

    pulls first to ensure sync

--- git-pusher.sh
+++ git-pusher.sh
@@ -1,4 +1,5 @@
 commit_count=$(git rev-list --count HEAD)
+git pull
 echo "The total number of commits is: $commit_count" > commit_counter.txt
 git add commit_counter.txt
 git commit -m "This is commit $commit_count"
